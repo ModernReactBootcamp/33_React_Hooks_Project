582c2b3f986f8e83d0275bcd71efdea4e6c9ddd9
kdord <katya.koliada49@gmail.com>
2020-01-17 14:57:47 +0200

    add TodoApp func component with appbar

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Grid from '@material-ui/core/Grid';
 
 export default function TodoApp() {
+  const initialTodos = [
+    { id: 1, task: 'Clean fishtank', completed: false },
+    { id: 2, task: 'Wash Car', completed: false },
+    { id: 3, task: 'Wash dishes', completed: false }
+  ];
+  const [todos, setTodos] = useState(initialTodos);
   return (
-    <div>
-      <h1>TODO</h1>
-    </div>
+    <Paper
+      style={{
+        padding: 0,
+        margin: 0,
+        height: '100vh',
+        backgroundColor: '#fafafa'
+      }}
+      elevation={0}
+    >
+      <AppBar color='primary' position='static' style={{ height: '64px' }}>
+        <Toolbar>
+          <Typography color='inherit'>TODOS WITH HOOKS</Typography>
+        </Toolbar>
+      </AppBar>
+      {/* <TodoList todos={todos} ></TodoList> */}
+    </Paper>
   );
 }
+
+// - TodoApp
+//   - TodoForm
+//      - TodoList
+//          - TodoItem
+
+// id, task, completed
