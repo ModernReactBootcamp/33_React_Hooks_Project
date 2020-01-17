9f6885e11219239febe3eaa3b8067ff9b20dc23a
kdord <katya.koliada49@gmail.com>
2020-01-17 18:43:02 +0200

    add Toggling and Deletion w/ Hooks

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -7,20 +7,30 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 
-export default function TodoItem({ task, completed }) {
+export default function TodoItem({
+  task,
+  completed,
+  removeTodo,
+  id,
+  toggleTodo
+}) {
   return (
     <ListItem>
-      <CheckBox tabIndex={-1} checked={completed} />
+      <CheckBox
+        tabIndex={-1}
+        checked={completed}
+        onClick={() => toggleTodo(id)}
+      />
       <ListItemText
         style={{ textDecoration: completed ? 'line-through' : 'none' }}
       >
         {task}
       </ListItemText>
       <ListItemSecondaryAction>
-        <IconButton aria-label='Delete'>
+        <IconButton aria-label='Edit'>
           <EditIcon />
         </IconButton>
-        <IconButton aria-label='Edit'>
+        <IconButton aria-label='Delete' onClick={() => removeTodo(id)}>
           <DeleteIcon />
         </IconButton>
       </ListItemSecondaryAction>
--- src/TodoList.js
+++ src/TodoList.js
@@ -4,16 +4,19 @@ import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import TodoItem from './TodoItem';
 
-export default function TodoList(props) {
+export default function TodoList({ todos, removeTodo, toggleTodo }) {
   return (
     <Paper>
       <List>
-        {props.todos.map(todo => (
+        {todos.map(todo => (
           <>
             <TodoItem
               task={todo.task}
+              id={todo.id}
               key={todo.id}
               completed={todo.completed}
+              removeTodo={removeTodo}
+              toggleTodo={toggleTodo}
             />
             <Divider />
           </>
