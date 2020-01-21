5d885c31c7536f48eacb0c253c376d6a79d13ba4
kdord <katya.koliada49@gmail.com>
2020-01-21 13:50:25 +0200

    add todoContext

--- src/contexts/todos.context.js
+++ src/contexts/todos.context.js
@@ -1,7 +1,8 @@
 //todos
 // all methods to interact w/ todos
 
-import React, { createContext } from 'react';
+import React, { createContext, useReducer } from 'react';
+import todoReducer from '../reducers/todo.reducer';
 import useTodoState from '../hooks/useTodoState';
 
 const defaultTodos = [
@@ -10,12 +11,15 @@ const defaultTodos = [
 ];
 
 export const TodosContext = createContext();
+export const DispatchContext = createContext();
 
 export function TodosProvider(props) {
-  const todosStuff = useTodoState(defaultTodos);
+  const [todos, dispatch] = useReducer(todoReducer, defaultTodos);
   return (
-    <TodosContext.Provider value={todosStuff}>
-      {props.children}
+    <TodosContext.Provider value={todos}>
+      <DispatchContext.Provider value={dispatch}>
+        {props.children}
+      </DispatchContext.Provider>
     </TodosContext.Provider>
   );
 }
